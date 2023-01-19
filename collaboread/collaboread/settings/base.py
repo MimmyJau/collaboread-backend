48f20960d8eec5f5a4ccb02ec5be96c30273d1d3
Jimmy <jimmy.b.mau@gmail.com>
2023-01-19 13:29:00 -0500

    Refactor settings for distinct local and prod

--- collaboread/collaboread/settings/base.py
+++ collaboread/collaboread/settings/base.py
@@ -69,20 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'collaboread.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
---
+++ collaboread/collaboread/settings/local.py
@@ -0,0 +1,14 @@
+from collaboread.settings.base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
