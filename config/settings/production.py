f3c05c2e88b3ae6041cfa989eac3a3d5ded02a8f
Jimmy <jimmy.b.mau@gmail.com>
2023-02-06 12:08:20 -0500

    Clean product/serializers.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,12 +10,5 @@ ALLOWED_HOSTS = ['collaboread.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
