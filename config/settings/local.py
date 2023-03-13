ad34a360aaf7ce76ee39a72e9c7f1c538b656853
Jimmy <jimmy.b.mau@gmail.com>
2023-03-13 16:05:39 -0400

    Refactor settings/local.py with black

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,11 +4,10 @@ from config.settings.base import *
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
