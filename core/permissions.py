743c37cb4ff7a60c7b9c2139bbe5207912be4bd4
Jimmy <jimmy.b.mau@gmail.com>
2023-05-16 13:37:10 -0400

    Document IsOwnerOrReadOnly behvaiour
    Noting that it does not get automatically called on generic views that
    return lists (e.g. ones that use .get_queryset)
    
    Rather, it will be called in views that use .get_object. In the event
    that we want to customize a .get_object method by overriding it, we must
    explicitly call .check_object_permissions to run a method like
    .has_object_permissions in IsOwnerOrReadOnly.

--- core/permissions.py
+++ core/permissions.py
@@ -7,6 +7,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
+        """
+        According to docs, .has_object_permission() is not called
+        automatically on every object when a list is returned, thus
+        this method is not called for list views by default.
+        Source: https://www.django-rest-framework.org/api-guide/permissions/#limitations-of-object-level-permissions
+        """
         # Read permissions are allowed for any request,
         # so we'll always allow GET, HEAD, or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
