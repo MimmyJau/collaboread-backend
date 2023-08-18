fcd516ed4c5acf422aa2f07958396166082c0fe4
Jimmy <jimmy.b.mau@gmail.com>
2023-08-18 14:05:56 -0400

    refactor: reorder urls in URLConf

--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,12 @@ urlpatterns = [
     path("articles/", views.article_list_view, name="articles"),
     path("toc/<slug_full>/", views.table_of_contents_retrieve_view, name="toc"),
     path(
+        "articles/<path:slug_full>/", views.article_retrieve_update_view, name="article"
+    ),
+    path(
         "articles/<path:slug_full>/annotations/",
         views.annotation_list_create_view,
         name="annotations",
-    ),
-    path(
-        "articles/<path:slug_full>/", views.article_retrieve_update_view, name="article"
     ),
     path(
         "annotations/<uuid>/",
