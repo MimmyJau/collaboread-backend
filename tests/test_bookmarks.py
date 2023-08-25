f447ad43ed669d2163cc704e3cfb74c4c8f295a3
Jimmy <jimmy.b.mau@gmail.com>
2023-08-25 16:45:26 -0400

    test: minor changes & remove bookmark create tests

--- tests/test_bookmarks.py
+++ tests/test_bookmarks.py
@@ -1,5 +1,7 @@
 from rest_framework.test import APITestCase
 
+BASE_URL = "http://localhost:8000"
+
 AUTH_BASE_URL = f"{BASE_URL}/auth"
 REGISTRATION_URL = f"{AUTH_BASE_URL}/registration/"
 LOGIN_URL = f"{AUTH_BASE_URL}/login/"
@@ -8,6 +10,8 @@ API_BASE_URL = f"{BASE_URL}/api"
 ARTICLE_CREATE_ROOT_URL = f"{API_BASE_URL}/articles/add-root/"
 ARTICLE_LIST_URL = f"{API_BASE_URL}/articles/"
 ARTICLE_DETAIL_URL = f"{API_BASE_URL}/articles"
+
+BOOKMARK_CREATE_URL = f"{API_BASE_URL}/bookmarks/"
 
 valid_user_payload = {
     "username": "testuser",
@@ -32,42 +36,91 @@ valid_article_payload = {
 }
 
 
-class BookmarkCreateTest(APITestCase):
-    def test_successful_bookmark_create_by_user(self):
-        pass
+def generate_bookmark_payload(article_path, index):
+    return {
+        "article": article_path,
+        "highlight": [
+            {
+                "characterRange": {
+                    "start": index,
+                    "end": index,
+                }
+            }
+        ],
+    }
 
-    def test_unsuccessful_bookmark_create_another_bookmark_by_user(self):
-        # unclear what should happen here.
-        # either 1) old bookmark is deleted or 2) new bookmark is not created.
-        pass
+
+class BookmarkCreateTest(APITestCase):
+    """
+    All Bookmark create tests should be unsuccessful.
+    To successfully create a new Bookmark, use PUTAsCreate.
+    """
+
+    def setUp(self):
+        # Create user.
+        response = self.client.post(REGISTRATION_URL, valid_user_payload)
+        self.token = response.data["key"]
+        # Login.
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        # Create article.
+        response = self.client.post(ARTICLE_CREATE_ROOT_URL, valid_article_payload)
+        self.article_path = response.data["slug_full"]
+        # Logout.
+        self.client.credentials()
+
+    def test_unsuccessful_bookmark_create_by_user(self):
+        # Login.
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        # Create bookmark.
+        valid_bookmark_payload = generate_bookmark_payload(self.article_path, 5)
+        response = self.client.post(BOOKMARK_CREATE_URL, valid_bookmark_payload)
+        self.assertEqual(response.status_code, 405)
 
     def test_unsuccessful_bookmark_create_by_nonuser(self):
-        pass
-
-    def test_unsuccessful_bookmark_create_missing_book_field(self):
-        pass
-
-    def test_unsuccessful_bookmark_create_missing_article_field(self):
-        pass
-
-    def test_unsuccessful_bookmark_create_missing_highlight_field(self):
-        pass
-
-    def test_unsuccessful_bookmark_create_highlight_start_neq_highlight_end(self):
-        pass
+        # Create bookmark.
+        valid_bookmark_payload = generate_bookmark_payload(self.article_path, 5)
+        response = self.client.post(BOOKMARK_CREATE_URL, valid_bookmark_payload)
+        self.assertEqual(response.status_code, 405)
 
 
 class BookmarkUpdateTest(APITestCase):
-    def test_successful_bookmark_update_by_user(self):
-        pass
+    def setUp(self):
+        # Create user.
+        response = self.client.post(REGISTRATION_URL, valid_user_payload)
+        self.token = response.data["key"]
+        # Login.
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        # Create article.
+        response = self.client.post(ARTICLE_CREATE_ROOT_URL, valid_article_payload)
+        self.article_path = response.data["slug_full"]
+        # Set URL path
+        self.BOOKMARK_UPDATE_URL = f"{API_BASE_URL}/bookmark/{self.article_path}/"
+        # Logout.
+        self.client.credentials()
 
     def test_successful_bookmark_PUTAsCreate_by_user(self):
+        # Login.
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        # PUTAsCreate bookmark.
+        valid_bookmark_payload = generate_bookmark_payload(self.article_path, 5)
+        response = self.client.put(
+            self.BOOKMARK_UPDATE_URL, valid_bookmark_payload, format="json"
+        )
+        print(response.data)
+        self.assertEqual(response.status_code, 201)
+
+        pass
+
+    def test_successful_bookmark_update_by_user(self):
         pass
 
     def test_unsuccessful_bookmark_update_by_another_user(self):
         pass
 
     def test_unsuccessful_bookmark_update_by_nonuser(self):
+        pass
+
+    def test_unsuccessful_bookmark_update_for_nonexistent_book(self):
         pass
 
     def test_unsuccessful_bookmark_update_missing_book_field(self):
